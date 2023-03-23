caeff9c6c8463349ecc2e9fa5bdd5734391e193f
Boris <b.todorov91@gmail.com>
2023-03-23 18:30:45 +0200

    new

---
+++ src/src/Lab/workingWithAbstraction/HotelReservation/DiscountType.java
@@ -0,0 +1,22 @@
+package Lab.workingWithAbstraction.HotelReservation;
+
+public enum DiscountType {
+    VIP(20),
+    SECONDVISIT(10),
+    NONE(0)
+    ;
+
+    private double percent;
+
+    DiscountType(double percent) {
+        this.percent = percent;
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+
+    public void setPercent(double percent) {
+        this.percent = percent;
+    }
+}
---
+++ src/src/Lab/workingWithAbstraction/HotelReservation/PriceCalculator.java
@@ -0,0 +1,14 @@
+package Lab.workingWithAbstraction.HotelReservation;
+
+public class PriceCalculator {
+
+    public static double calculateHolidayPrice(double priceForDay, int numOfDay, Season season, DiscountType discountType){
+
+        double calculateAllPrice = priceForDay * numOfDay;
+
+        calculateAllPrice = calculateAllPrice * season.getCoefficient();
+        calculateAllPrice = calculateAllPrice - ( calculateAllPrice * (discountType.getPercent() / 100));
+
+        return calculateAllPrice;
+    }
+}
