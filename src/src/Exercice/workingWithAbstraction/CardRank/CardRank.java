e012b867d1f598ef44aa4ff16211a39586aa90b7
Boris <b.todorov91@gmail.com>
2023-03-24 17:09:46 +0200

    new

---
+++ src/src/Exercice/workingWithAbstraction/CardRank/CardRank.java
@@ -0,0 +1,17 @@
+package Exercice.workingWithAbstraction.CardRank;
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+}
