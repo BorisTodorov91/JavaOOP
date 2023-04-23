89eabf3fae6918d205ed6106bdcf7002c268cc57
Boris <b.todorov91@gmail.com>
2023-04-23 21:27:11 +0300

    new

--- src/src/Exercice/P03inheritance/restaurant/Main.java
+++ src/src/Exercice/P03inheritance/restaurant/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Cake cake = new Cake("Dobos", 123, 23, 233);
+        Cake cake = new Cake("Dobos");
+
+        System.out.println(cake.getCalories());
     }
 }
